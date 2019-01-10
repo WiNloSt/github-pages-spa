134ef8454d1e44d50e8ec554539b95af98e06e71
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2019-01-10 17:35:54 +0700

    Updates

---
+++ precache-manifest.f0920b92edd6a28a1468430e23df1841.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f0bff0039a6a6d67f0bb",
+    "url": "/static/js/main.f0bff003.chunk.js"
+  },
+  {
+    "revision": "43b6126f2243a9d8652f",
+    "url": "/static/js/1.43b6126f.chunk.js"
+  },
+  {
+    "revision": "f0bff0039a6a6d67f0bb",
+    "url": "/static/css/main.38064753.chunk.css"
+  },
+  {
+    "revision": "78b347b0ee502f7410f9957dbd2ec048",
+    "url": "/index.html"
+  }
+];
